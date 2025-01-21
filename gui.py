c544568e1e65fe0f215141dfd62c1e547902e452
Adalarus <adalarus@gmail.com>
2025-01-21 20:33:13 +0500

    Интерфейс в черновом варианте. Надо делать обработку кнопок.

--- gui.py
+++ gui.py
@@ -1,24 +1,54 @@
+"""
+Этот модуль предназначен для вывода псевдографики в терминале для программы ski-py.
+"""
 from rich import console, table
 from rich.console import Console
 from rich.table import Table
+import data
 
 class GUI_status:
-    title = ""
-    sh_list = []
-    table = []
-    curX = 0
+    """
+        Класс предназначен для изменения состояния интерфейса. 
+        При измении полей для отображения экземпляр этого класса передается функции GUI_show
+    """
+    title = "" #Заголовок таблицы
+    sh_list = [] #Список школ
+    table = [] #Таблица содержащая все данные
+    frame_table = []#Часть таблицы, начинающаяся с переменной frame_set и величиной 10 элементов
+    frame_set = 0
+    curX = 0 #Позиция указателя
     curY = 0
     hint1 = ""
     hint2 = ""
     
+    def __init__(self) -> None:
+        self.sh_list = data.Get_sh_list()
+        self.table = data.Get_table()
+        self.set_frame()
+        self.hint1 = "Верхний текст"
+        self.hint2 = "Нижний текст"
+        self.title = "Заголовок"
+    
+    def set_frame(self,frame = 0):
+        """
+        Метод смещает начало списка к указанной позиции.
 
-
+        """
+        self.frame_set = frame
+        self.frame_table = []
+        for i in range(frame, frame + 30):
+            if(i > (len(self.table) - 1)):
+                break
+            self.frame_table.append(self.table[i])
 
 
 console = Console()
 
 def GUI_show(status: GUI_status):
-
+    """
+    Функция отображает интерфейс программы в зависимости от объекта переменной GUI_status
+    """
+    console.clear()
     table = Table(title=status.title)
 
     table.add_column("Номер")
@@ -28,9 +58,17 @@ def GUI_show(status: GUI_status):
     table.add_column("Время финиша")
     table.add_column("Чистое время")
     table.add_column("Метка")
+    
+    for j, v in enumerate(status.frame_table):
+        list = []
+        temp = ""
+        for i, x in enumerate(v):
+            temp = "[white on black]"
+            if(j == status.curY and i == status.curX):
+                temp = "[black on white]"
+            list.append(temp + x)
 
-
-    table.add_row("1","Иванов Иван" ,"Школа #0", "00:20'00" ,"00:34'26", "00:14'26", "М12")
+        table.add_row(list[0], list[1],list[2],list[3],list[4],list[5],list[6])
 
     console.print(status.hint1)
     console.print(table)
--- main.py
+++ main.py
@@ -7,5 +7,8 @@
 import gui
 
 status_gui = gui.GUI_status()
+status_gui.curY = 5
+status_gui.curX = 1
+status_gui.set_frame(0)
 gui.GUI_show(status_gui)
 input()
