fbec4ff841a181ab9ced02c72931f55d93c3057e
Adalarus <adalarus@gmail.com>
2025-01-25 06:34:57 +0500

    Реализовано перемещение курсора

--- core.py
+++ core.py
@@ -4,8 +4,11 @@ import gui
 
 main_gui = gui.GUI_status()
 gui.GUI_show(main_gui)
+
 class Core():
     def OnKeyPressed(self, key):
+        
+        
         match key:
             case keys.ESC:
                 exit()
@@ -17,7 +20,25 @@ class Core():
                    gui.GUI_show(main_gui)
                    return
                 main_gui.curY +=1
-                gui.GUI_show(main_gui)
             case keys.UP:
+                if(main_gui.frame_set + main_gui.curY == 0):
+                    return
+                if(main_gui.curY == 0):
+                    main_gui.set_frame(main_gui.frame_set - 1)
+                    gui.GUI_show(main_gui)
+                    return
                 main_gui.curY -=1
                 gui.GUI_show(main_gui)
+            case keys.RIGHT:
+                if(main_gui.curX == 6):
+                    return
+                main_gui.curX += 1
+            case keys.LEFT:
+                if(main_gui.curX == 0):
+                    return
+                main_gui.curX -=1
+
+
+
+        
+        gui.GUI_show(main_gui)
